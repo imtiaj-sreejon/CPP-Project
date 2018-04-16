6b1bfd55fd4cd19973ab0888b3c9cd1c861df2d3
Imtiaj Sreejon <imtiajahmed@iut-dhaka.edu>
2018-04-16 23:32:32 +0600

    Add files via upload
    created student class

---
+++ student.cpp
@@ -0,0 +1,37 @@
+#include "stdafx.h"
+#include "student.h"
+
+
+
+
+student::student(int _id, string _name, int _is_res, int _credit_card)
+{
+	id = _id;
+	name = _name;
+	is_res = _is_res;
+	credit_card = _credit_card;
+}
+
+int student::get_id()
+{
+	return id;
+}
+
+string student::get_name()
+{
+	return name;
+}
+
+int student::get_is_res()
+{
+	return is_res;
+}
+
+int student::get_credit_card()
+{
+	return credit_card;
+}
+
+student::~student()
+{
+}
---
+++ student.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <string>
+using namespace std;
+
+class student
+{
+	int id;
+	string name;
+	int is_res;
+	int credit_card;
+public:
+	student(int _id, string _name, int _is_res, int _credit_card);
+	int get_id();
+	string get_name();
+	int get_is_res();
+	int get_credit_card();
+	~student();
+};
+
