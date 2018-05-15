8192c54c6a75b05a61ec3d58384a9cc8a44a02ba
Rezwan-Muid <38012417+Rezwan-Muid@users.noreply.github.com>
2018-05-15 22:14:54 +0600

    Add files via upload

---
+++ control_functions.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "stdafx.h"
+using namespace std;
+
+void menu();
+void master(string name, string passwd);
+void admin_func(string name, string passwd); // will perform admin functions
