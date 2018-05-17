a16f2069e39544bfbcd7838baf6847086be438c6
Rezwan-Muid <38012417+Rezwan-Muid@users.noreply.github.com>
2018-05-17 23:13:44 +0600

    programmed database check

---
+++ database_check.cpp
@@ -0,0 +1,61 @@
+#include "stdafx.h"
+#include "database_check.h"
+#include <SQLAPI.h>
+#include <ora7API.h>
+#include <oraAPI.h>
+using namespace std;
+
+string check_admin(string name)
+{
+	SAConnection con;
+	try
+	{
+		// connect to database
+		con.Connect(
+			"XE",     // database name
+			"cpp_proj",   // user name
+			"test123",   // password
+			SA_Oracle_Client);
+		SACommand cmd(&con);
+
+		// check if admin exists and return password
+		// prototype: check_admin(ad_name in varchar2, passwd out varchar2)
+		cmd.setCommandText("check_admin");
+		cmd.Param("ad_name").setAsString() = name.c_str(); // pass parameters
+
+		cmd.Execute();
+
+		// get the pass from the check procedure if admin exists
+		// procedure will return "not found" if admin doesn't exist
+		string pass = string(cmd.Param("passwd").asString());
+
+		if (pass == "not found")
+		{
+			cout << "admin doesn't exist\n";
+			con.Disconnect(); // disconnect before return
+			return pass;
+		}
+		else
+		{
+			con.Disconnect();
+			return pass;
+		}
+	}
+	catch (SAException &x)
+	{
+		// SAConnection::Rollback()
+		try
+		{
+			// on error rollback changes
+			cout << "rolling back.....";
+			con.Rollback();
+		}
+		catch (SAException &)
+		{
+		}
+		// print error message
+		printf("%s\n", (const char*)x.ErrText());
+	}
+}
+
+
---
+++ database_check.h
@@ -0,0 +1,5 @@
+#pragma once
+#include "stdafx.h"
+using namespace std;
+
+string check_admin(string name); // returns password if admin exists
