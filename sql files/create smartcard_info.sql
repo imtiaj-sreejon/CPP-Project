7a30016a64f4827d1edcbb58689a11c6e6578f7c
Ziad Karim <38079711+Ziadkarim84@users.noreply.github.com>
2018-05-18 03:06:46 +0600

    created student table and updated smartcard_info table

--- sql files/create smartcard_info.sql
+++ sql files/create smartcard_info.sql
@@ -2,5 +2,7 @@
 	id number,
 	balance number default 500,
 	constraints pk_credit primary key(id),
+	constraints fk_card_student foreign key(student_id) references student,
+	constraints uni_std_id unique(student_id),
 	constraints check_balance check(balance > 199)
 );
---
+++ sql files/create_student.sql
@@ -0,0 +1,6 @@
+﻿CREATE table student(
+	id number,
+	name varchar2(20),
+	is_res number,
+	constraints pk_student primary key(id)
+);
