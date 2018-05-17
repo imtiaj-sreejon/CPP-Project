5d79cf310f5877f2162810bf3b85c397e2a6c792
Imtiaj Sreejon <imtiajahmed@iut-dhaka.edu>
2018-05-18 00:53:00 +0600

    created food_menu table

---
+++ sql files/create_food_menu.sql
@@ -0,0 +1,7 @@
+﻿create table food_menu(
+	day varchar2(20),
+	meal_time varchar2(20),
+	items varchar2(100),
+	constraints check_day check(day in ('saturday','sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday')),
+	constraints check_meal_time check(meal_time in ('breakfast', 'lunch', 'dinner'))
+);
