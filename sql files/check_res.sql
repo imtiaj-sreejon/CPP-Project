54514a629d8e5d6826fb14f1ee21c2354062728b
Hafiz Tushar <37898843+hafiztushar@users.noreply.github.com>
2018-05-18 05:43:33 +0600

    implemented check_res pl/sql procedure

---
+++ sql files/check_res.sql
@@ -0,0 +1,9 @@
+﻿CREATE or replace procedure check_res(input_id in number, chk out number) as
+	temp number;
+begin
+	select is_res into temp
+	from student
+	WHERE id = input_id;
+	chk:=temp;
+end;
+/
