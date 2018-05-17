43ebffcd8d41513c1a31ca8a2c1efe529557e755
Ziad Karim <38079711+Ziadkarim84@users.noreply.github.com>
2018-05-17 19:22:10 +0600

    modified check admin sql

---
+++ check_admin.sql
@@ -0,0 +1,13 @@
+﻿CREATE or replace procedure check_admin(input_id in number, x out varchar2) as
+	temp varchar2(20);
+begin
+	select name into temp
+	from admin
+	WHERE id = input_id;
+	x:=temp;
+	exception
+	when no_data_found
+	then x:='not found';
+end;
+
+
