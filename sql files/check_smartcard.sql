50ffa562efa96aef397a4aa6a920b4730e42a09c
Lesan01 <38044640+Lesan01@users.noreply.github.com>
2018-05-18 08:12:20 +0600

    implemented check smartcard sql

---
+++ sql files/check_smartcard.sql
@@ -0,0 +1,12 @@
+﻿CREATE or replace procedure check_smartcard(input_id in number, chk out number) as
+	temp number;
+begin
+	select id into temp
+	from smartcard_info
+	WHERE student_id = input_id;
+	chk:=temp;
+	exception
+	when no_data_found
+	then chk:=0;
+end;
+/
